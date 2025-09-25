462f5dc16e2389bfb527c0fffceeb9f8904a11dc
nirja <nirjadabhi34@gmail.com>
2025-09-24 23:49:10 -0400

    api 1

---
+++ 101509539_COMP3123-exec4/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+
+const SERVER_PORT = 3000;
+var app = express();
+
+
+// GET /hello
+app.get('/hello', function(req, res) {
+    res.send('hellooowww');
+});
+
+// Returns Hello Express JS as plain text.
+
+// GET /user (query parameters)
+
+// Accepts query string parameters: ?firstname=<value>&lastname=<value>.
+
+// If parameters are not provided, default to firstname = "Pritesh" and lastname = "Patel".
+// Example request: GET /user?firstname=John&lastname=Doe
+
+// Example response:
+
+// { "firstname": "John", "lastname": "Doe" }
+// POST /user/:firstname/:lastname (path parameters)
+
+// Expects path parameters for firstname and lastname.
+
+// Example request: POST /user/John/Doe
+
+// Example response:
+
+// { "firstname": "John", "lastname": "Doe" }
+// POST /users (body – array of users)
+
+// Expects a JSON array in the request body, each item with firstname and lastname.
+
+
+
